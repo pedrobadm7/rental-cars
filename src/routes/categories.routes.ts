1cbdd7eb61ed542d70bb6509f5ef1d3a5bcffafd
Pedro Barros <pedrobars7a@gmail.com>
2022-09-14 16:59:19 -0300

    Inserindo tipagem

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,24 @@
+import { Request, Response, Router } from 'express';
+import { Category } from '../model/Category';
+
+const categoriesRoutes = Router();
+
+const categories: Category[] = [];
+
+categoriesRoutes.post('/', (request: Request, response: Response) => {
+  const { name, description } = request.body;
+
+  const category: Category = new Category();
+
+  Object.assign(category, {
+    name,
+    description,
+    created_at: new Date(),
+  });
+
+  categories.push(category);
+
+  return response.status(201).json(category);
+});
+
+export { categoriesRoutes };
