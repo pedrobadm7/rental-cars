791f7ec7d0a4b023a0e1cf6791322cffba05b95a
Pedro Barros <pedrobars7a@gmail.com>
2022-09-14 17:26:29 -0300

    Listando categorias

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -12,4 +12,10 @@ categoriesRoutes.post('/', (request: Request, response: Response) => {
   return response.status(201).send();
 });
 
+categoriesRoutes.get('/', (request: Request, response: Response) => {
+  const allCategories = categoriesRepository.list();
+
+  return response.json({ allCategories });
+});
+
 export { categoriesRoutes };
