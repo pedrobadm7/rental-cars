1a905002bde1e80849e7f567f934ac7764eff367
Pedro Barros <pedrobars7a@gmail.com>
2022-09-20 20:31:07 -0300

    Criando use case de especificação

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,22 +1,10 @@
-import { Request, Response, Router } from 'express';
-import { SpecificationsRepository } from '../modules/cars/repositories/implementations/SpecificationsRepository';
-
-import { CreateSpecificationService } from '../modules/cars/services/CreateSpecificationService';
+import { Router } from 'express';
+import { createSpecificationController } from '../modules/cars/useCases/createSpecification';
 
 const specificationRoutes = Router();
 
-const specificationsRepository = new SpecificationsRepository();
-
-specificationRoutes.post('/', (request: Request, response: Response) => {
-  const { name, description } = request.body;
-
-  const createSpecificationService = new CreateSpecificationService(
-    specificationsRepository,
-  );
-
-  createSpecificationService.execute({ name, description });
-
-  return response.status(201).send();
+specificationRoutes.post('/', (request, response) => {
+  return createSpecificationController.handle(request, response);
 });
 
 export { specificationRoutes };
