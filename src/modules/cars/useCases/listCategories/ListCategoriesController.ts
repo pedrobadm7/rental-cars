9a18c746ccecc02fefbb18f4e2aef0823d457176
Pedro Barros <pedrobars7a@gmail.com>
2022-09-20 19:43:18 -0300

    Refatorando a listagem de categoria

---
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+
+import { ListCategoriesUseCase } from './ListCategoriesUseCase';
+
+class ListCategoriesController {
+  private listCategoriesUseCase: ListCategoriesUseCase;
+
+  constructor(listCategoriesUseCase: ListCategoriesUseCase) {
+    this.listCategoriesUseCase = listCategoriesUseCase;
+  }
+
+  handle(request: Request, response: Response) {
+    const allCategories = this.listCategoriesUseCase.execute();
+
+    return response.json({ allCategories });
+  }
+}
+
+export { ListCategoriesController };
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,15 @@
-import { Request, Response, Router } from 'express';
-import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
+import { Router } from 'express';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request, response) => {
   return createCategoryController.handle(request, response);
 });
 
-categoriesRoutes.get('/', (request: Request, response: Response) => {
-  const allCategories = categoriesRepository.list();
-
-  return response.json({ allCategories });
+categoriesRoutes.get('/', (request, response) => {
+  return listCategoriesController.handle(request, response);
 });
 
 export { categoriesRoutes };
