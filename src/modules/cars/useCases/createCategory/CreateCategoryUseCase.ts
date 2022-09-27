b6d422be622c7d2263b0e762423467dfc37539fb
Pedro Barros <pedrobars7a@gmail.com>
2022-09-27 08:14:57 -0300

    Conhecendo o TSyringe

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,3 +1,4 @@
+import { inject, injectable } from 'tsyringe';
 import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
 
 interface IRequest {
@@ -5,10 +6,14 @@ interface IRequest {
   description: string;
 }
 
+@injectable()
 class CreateCategoryUseCase {
   private categoriesRepository: ICategoriesRepository;
 
-  constructor(categoriesRepository: ICategoriesRepository) {
+  constructor(
+    @inject('CategoriesRepository')
+    categoriesRepository: ICategoriesRepository,
+  ) {
     this.categoriesRepository = categoriesRepository;
   }
 
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 /* eslint-disable no-console */
+import 'reflect-metadata';
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import swaggerFile from './swagger.json';
 import { router } from './routes';
-
 import './database';
+
+import './shared/container';
 
 const app = express();
 
