a7998e324a76273b9e97769cac27fe16b3fbabb0
Pedro Barros <pedrobars7a@gmail.com>
2022-10-09 11:59:18 -0300

    Autenticação nas rotas

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,39 @@
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+import { UsersRepository } from '../modules/accounts/repositories/implementations/UsersRepository';
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new Error('Token is missing');
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      '9d8e54630b531af9c4f12ae920ad4623',
+    ) as IPayload;
+
+    const usersRepository = new UsersRepository();
+    const user = await usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error('User does not exists!');
+    }
+
+    next();
+  } catch {
+    throw new Error('Invalid token!');
+  }
+}
