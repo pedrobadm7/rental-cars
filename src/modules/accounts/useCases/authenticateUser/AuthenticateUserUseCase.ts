d50f8c56a559bcac0ed572412868fdecd544fe15
Pedro Barros <pedrobars7a@gmail.com>
2022-10-09 12:27:29 -0300

    Tratamento de exceções

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -1,6 +1,7 @@
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import { inject, injectable } from 'tsyringe';
+import { AppError } from '../../../../errors/AppError';
 import { IUsersRepository } from '../../repositories/IUsersRepository';
 
 interface IRequest {
@@ -31,13 +32,13 @@ class AuthenticateUserUseCase {
     const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
-      throw new Error('User or password incorrect');
+      throw new AppError('Email or password incorrect');
     }
 
     const passwordMatch = await compare(password, user.password);
 
     if (!passwordMatch) {
-      throw new Error('User or password incorrect');
+      throw new AppError('Email or password incorrect');
     }
 
     const token = sign({}, '9d8e54630b531af9c4f12ae920ad4623', {
