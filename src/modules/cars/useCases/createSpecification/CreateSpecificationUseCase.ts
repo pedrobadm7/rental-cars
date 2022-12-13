f1098646506cda0230d637f81c6a7e8e6075e180
Pedro Barros <pedrobars7a@gmail.com>
2022-12-13 08:20:10 -0300

    Refatorando a aplicação

--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,5 +1,6 @@
+import { AppError } from '@shared/errors/AppError';
 import { inject, injectable } from 'tsyringe';
-import { AppError } from '../../../../errors/AppError';
+
 import { ISpecificationsRepository } from '../../repositories/ISpecificationsRepository';
 
 interface IRequest {
